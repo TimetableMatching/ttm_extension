c2eb10d21580ac190099db6f86d6e432b4f4c794
danny7742 <danny7742@naver.com>
2019-11-25 21:46:08 +0900

    TeamManage&withdrawal
    TeamManage&withdrawal button

---
+++ teamTableMatching/dashboard.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+    $('#withdrawal').click(function(){
+        alert("회원 탈퇴 되셨습니다.");
+    });
+
+    $('#dTeamManage').click(function(){
+        location.href="teamManage.html";
+    });
+});
