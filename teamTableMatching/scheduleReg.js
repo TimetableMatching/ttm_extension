6b1425716d6ff0d3b1aae3d49faac8305107cbda
youngjae <dudwo1344@gmail.com>
2019-11-29 20:38:34 +0900

    Create TeamPage

--- teamTableMatching/scheduleReg.js
+++ teamTableMatching/scheduleReg.js
@@ -1,25 +1,55 @@
 $(document).ready(function(){
     var scheduleArray = [];
+    var userInfo =  getUrlParams();
+
+
     $('td').click(function () {
         if( this.style.backgroundColor == "rgb(102, 102, 153)"){
             this.style.backgroundColor   = "white";
-            scheduleArray.forEach(removeValue);
-            
-
         }else{
             this.style.backgroundColor  = "rgb(102, 102, 153)";
-            scheduleArray.push($(this).attr("value"))
         }
-        console.log(scheduleArray);
     }); 
     $('#SignUpComplete').click(function () {
-        location.href = "login.html";
-        
-        //디비에 연동해서 회원가입정보, 시간표 넣으면됨
-    });
-    function removeValue(value, index, array) {
-        if($(this).attr("value") == value){
-            
+        var scheduleEntry = $('td');
+        for(var i= 0; i <scheduleEntry.length ; i++){
+            if( scheduleEntry[i].style.backgroundColor == "rgb(102, 102, 153)" ){
+                scheduleArray.push($(scheduleEntry[i]).attr("value"));
+                
+            }
         }
-      }
+        
+        var allData = {"email": userInfo.Id, "password":userInfo.Password, "name" : userInfo.Name, "organization":userInfo.Organization, "schedule" : scheduleArray };
+        var tmp = JSON.stringify(allData);
+        console.log(tmp);
+        // $.ajax({
+        //     type:"POST",
+        //     url: "http://13.209.50.215:5000/add_user",
+        //     data: tmp,
+        //     contentType  : "application/json",
+        //     cache : false,
+        //     processData: false,
+        //     success: function (data) {
+        //         console.log(data);
+        //         location.href="login.html";
+        //         alert("회원가입이 완료되었습니다.");
+        //     },error:function(data){
+        //         alert("error");
+        //         console.log(data);
+        //     }
+        // }); 
+    });
+    function getUrlParams() {
+        var params = {};
+        window.location.search.replace(/[?&]+([^=&]+)=([^&]*)/gi, function(str, key, value) { params[key] = value; });
+        return params;
+    }
+    function selectSchedule(item,idex){
+        
+        if( this.style.backgroundColor == "rgb(102, 102, 153)" ){
+            scheduleArray.push(item.attr("value"));
+        }
+        
+    }
+
 });
---
+++ teamTableMatching/team.js
@@ -0,0 +1,4 @@
+$(document).ready(function(){
+    //회원가입 버튼 클릭 시
+  
+});
